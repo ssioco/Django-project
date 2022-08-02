32431f85a42c880a4c4c22886c9e9030d9eceefb
ssioco <juanjo.9912@gmail.com>
2022-08-01 20:26:01 -0500

    Proyecto con modelos y relaciones

--- planta/admin.py
+++ planta/admin.py
@@ -1,5 +1,24 @@
 from django.contrib import admin
-from .models import Trabajador
+from .models import Categoria, Produccion, Producto, Trabajador
 
 # Register your models here.
-admin.site.register(Trabajador)
+
+class TrabajadorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'cedula', 'nombre', 'apellido', 'correo')
+    search_fields = ['nombre','apellido','cedula']
+
+admin.site.register(Trabajador, TrabajadorAdmin)
+admin.site.register(Categoria)
+
+class ProductoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nombre', 'ficha_tecnica', 'costo', 'Categoria','descripcionCategoria','color' )
+    list_filter = ['Categoria']
+    search_fields = ['nombre','Categoria__nombre']
+    list_editable = ['color']
+
+    def descripcionCategoria(self,obj):
+        return obj.Categoria.desc
+
+
+admin.site.register(Producto, ProductoAdmin)
+admin.site.register(Produccion)
--- planta/models.py
+++ planta/models.py
@@ -1,7 +1,10 @@
 from django.db import models
 
+#Makemigrations planta
+#Migrate 
+
 # Create your models here.
-class Trabajador(models.Model):
+class Trabajador(models.Model): #Herencia de el framework
     cedula = models.IntegerField(unique=True)
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
@@ -9,3 +12,35 @@ class Trabajador(models.Model):
 
     def __str__(self) -> str:
         return f"{self.nombre} {self.apellido}"
+
+class Categoria(models.Model):
+    nombre = models.CharField(max_length=100, unique = True)
+    desc = models.CharField(max_length=254)
+
+    def __str__(self) -> str:
+        return f"{self.nombre}"
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=100, unique = True)
+    ficha_tecnica = models.TextField()
+    costo = models.IntegerField()
+    Categoria = models.ForeignKey(Categoria, on_delete = models.DO_NOTHING)
+    COLOR = (
+        ('rojo','ROJO'),
+        ('verde','VERDE'),
+        ('azul','AZUL'),
+    )    
+    color = models.CharField(max_length=100, choices=COLOR, default='r')
+
+    def __str__(self) -> str:
+        return f"{self.nombre}"
+
+class Produccion(models.Model):
+    Trabajador = models.ForeignKey(Trabajador, on_delete = models.DO_NOTHING)
+    Producto = models.ForeignKey(Producto, on_delete = models.DO_NOTHING)
+    cantidad = models.IntegerField()
+    fecha_creacion = models.DateTimeField()
+
+    def __str__(self) -> str:
+        return f"{self.Producto} {self.cantidad}"
+    
