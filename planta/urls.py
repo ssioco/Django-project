feebb5f5644cc0f7f273b7fa78810d6df6ff369a
ssioco <juanjo.9912@gmail.com>
2022-08-08 20:56:55 -0500

    Index con navbar

--- planta/urls.py
+++ planta/urls.py
@@ -6,15 +6,7 @@ app_name ="planta" #Permite utilizar un tag para usar la app
 
 urlpatterns =[
     path('', views.index, name = "index"),
-    path('saludar/', views.saludar, name = "saludar"),
-    path('adios/', views.despedir, name = "despedir"),
-    path('saludo-especial/<str:nombre>', views.saludoEspecial, name = "especial"),
-    path('numero/<int:num>/<int:num2>', views.multiplo, name = "multiplo"),
-
-    path('login-fornulario/', views.loginFormulario, name = "loginf"),
-    path('login/', views.login, name = "login"),
-    path('sumita/', views.sumita, name = "sumita"),
-    path('sumitaRes/', views.sumitaResultado, name = "sumitaRes"),
+    
 
 
 
--- planta/views.py
+++ planta/views.py
@@ -8,37 +8,4 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'planta/index.html')
 
-def saludar(request):
-    return HttpResponse("Hola mundo")
 
-def despedir(request):
-    return HttpResponse("Adios prro <a href='http://127.0.0.1:8000/planta/saludar/'>Click aqui</a>")
-
-def saludoEspecial(request,nombre):
-    return HttpResponse(f"Hola {nombre}")
-
-def multiplo(request,num,num2):
-    return HttpResponse(f"Hola tu resultado es {num*num2}")
-
-def loginFormulario(request):
-    return render(request, 'planta/login/login-form.html')
-
-def login(request):
-    u= request.POST["user"]
-    c= request.POST["pswd"]
-
-    if u == "Juan" and c == "12345":
-        return HttpResponse("bienvenido mi rey")
-    else:
-        return HttpResponse("no eres bienvenido mi rey")
-
-def sumita(request):
-    return render(request, 'planta/suma/suma.html')
-
-def sumitaResultado(request):
-    a= int(request.POST["valor1"])
-    b= int(request.POST["valor2"])
-
-    r=a+b
-
-    return HttpResponse(f"Resultado: {r}")
